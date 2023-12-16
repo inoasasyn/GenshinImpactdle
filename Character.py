5c7403edb72a3bba06b7fe087e44fde5e5987d70
s464920 <marnow94@st.amu.edu.pl>
2023-12-16 13:20:06 +0100

    fully working in console

--- Character.py
+++ Character.py
@@ -1,13 +1,13 @@
 class Character:
 
-    def __init__(self, name, rarity, element, weapon_type, region, model_type, special_stat,
+    def __init__(self, name, rarity, element, weapon_type, model_type, region, special_stat,
                  ascension_local_speciality, ascension_boss_drop, ascension_mob_drop, release_date, version):
         self.name = name
         self.rarity = rarity
         self.element = element
         self.weapon_type = weapon_type
-        self.region = region
         self.model_type = model_type
+        self.region = region
         self.special_stat = special_stat
         self.ascension_local_speciality = ascension_local_speciality
         self.ascension_boss_drop = ascension_boss_drop
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import setup
 import finder
 import random
+from datetime import datetime
 
 
 def game():
     characters = setup.scrap_all_characters()
     while True:
+        print("Guess character!")
         guess_counter = 0
         max_guesses = 5
-        number_of_additional_properties = 4
+        number_of_additional_properties = 3
         random_number = random.randint(0, len(characters)-1)
         character_to_guess = characters[random_number]
         properties = list(vars(character_to_guess).keys())
-        displayed_properties = properties[0:2]
-        while len(displayed_properties) < 2+number_of_additional_properties:
-            add_properties = random.choices(properties[2:], k=number_of_additional_properties)
+        displayed_properties = properties[0:3]
+        while len(displayed_properties) < 3+number_of_additional_properties:
+            add_properties = random.choices(properties[3:], k=number_of_additional_properties)
             if (properties[-1] or properties[-2] not in add_properties) and len(set(add_properties)) == number_of_additional_properties:
                 displayed_properties += add_properties
 
@@ -23,8 +25,25 @@ def game():
             guess_counter += 1
             guessed_character = finder.find_by_name(characters, guess)
             for x in displayed_properties:
-                print(getattr(character_to_guess, x))
-                print(getattr(guessed_character, x))
+                if x == "release_date" or x == "version":
+                    correct_date = getattr(character_to_guess, x)
+                    guessed_date = getattr(guessed_character, x)
+                    if x == "release_date":
+                        correct_date = datetime.strptime(correct_date, "%B %d, %Y")
+                        guessed_date = datetime.strptime(guessed_date, "%B %d, %Y")
+                    correct = correct_date == guessed_date
+                    if correct:
+                        print(str(x) + ": " + str(getattr(guessed_character, x)) + " is ", str(correct))
+                    elif correct_date > guessed_date:
+                        print(str(x) + ": " + str(getattr(guessed_character, x)) + " is OLDER")
+                    else:
+                        print(str(x) + ": " + str(getattr(guessed_character, x)) + " is NEWER")
+                else:
+                    correct = getattr(character_to_guess, x) == getattr(guessed_character, x)
+                    print(str(x) + ": " + str(getattr(guessed_character, x)) + " is ", str(correct))
+            if guessed_character == character_to_guess:
+                print("You WIN!!!")
+                guess_counter = max_guesses + 1
 
 
 game()
